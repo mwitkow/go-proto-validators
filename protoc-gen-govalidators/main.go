c4e2c34ec1aba31a560aa6e829a3c24730dee359
Michal Witkowski <michal@improbable.io>
2016-03-27 19:33:03 +0200

    the `protoc` plugin intself :)

---
+++ protoc-gen-govalidators/main.go
@@ -0,0 +1,52 @@
+// Copyright 2016 Michal Witkowski. All Rights Reserved.
+// See LICENSE for licensing terms.
+
+package main
+
+import (
+	"io/ioutil"
+	"os"
+	"strings"
+
+	"github.com/gogo/protobuf/proto"
+	"github.com/gogo/protobuf/protoc-gen-gogo/generator"
+	validator_plugin "github.com/mwitkow/go-proto-validators/plugin"
+)
+
+func main() {
+	gen := generator.New()
+
+	data, err := ioutil.ReadAll(os.Stdin)
+	if err != nil {
+		gen.Error(err, "reading input")
+	}
+
+	if err := proto.Unmarshal(data, gen.Request); err != nil {
+		gen.Error(err, "parsing input proto")
+	}
+
+	if len(gen.Request.FileToGenerate) == 0 {
+		gen.Fail("no files to generate")
+	}
+
+	gen.CommandLineParameters(gen.Request.GetParameter())
+
+	gen.WrapTypes()
+	gen.SetPackageNames()
+	gen.BuildTypeNameMap()
+	gen.GeneratePlugin(validator_plugin.NewPlugin())
+
+	for i := 0; i < len(gen.Response.File); i++ {
+		gen.Response.File[i].Name = proto.String(strings.Replace(*gen.Response.File[i].Name, ".pb.go", ".validator.pb.go", -1))
+	}
+
+	// Send back the results.
+	data, err = proto.Marshal(gen.Response)
+	if err != nil {
+		gen.Error(err, "failed to marshal output proto")
+	}
+	_, err = os.Stdout.Write(data)
+	if err != nil {
+		gen.Error(err, "failed to write output proto")
+	}
+}
