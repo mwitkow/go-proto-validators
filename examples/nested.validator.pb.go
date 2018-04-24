cf166d42aad32f60391f6881152d94d7d6837944
YARON SUMEL <YARON.SUMEL@SIMPLESURANCE.DE>
2018-04-24 11:15:06 +0200

    regenerate_example

--- examples/nested.validator.pb.go
+++ examples/nested.validator.pb.go
@@ -37,7 +37,6 @@ func (this *InnerMessage) Validate() error {
 
 var _regex_OuterMessage_ImportantString = regexp.MustCompile(`^[a-z]{2,5}$`)
 var _regex_OuterMessage_UserId = regexp.MustCompile(`^[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[4][a-fA-F0-9]{3}-[8|9|aA|bB][a-fA-F0-9]{3}-[a-fA-F0-9]{12}$`)
-var _regex_OuterMessage_UserIdX = regexp.MustCompile(`^[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[1-5][a-fA-F0-9]{3}-[8|9|aA|bB][a-fA-F0-9]{3}-[a-fA-F0-9]{12}$`)
 
 func (this *OuterMessage) Validate() error {
 	if !_regex_OuterMessage_ImportantString.MatchString(this.ImportantString) {
@@ -53,9 +52,6 @@ func (this *OuterMessage) Validate() error {
 	}
 	if !_regex_OuterMessage_UserId.MatchString(this.UserId) {
 		return go_proto_validators.FieldError("UserId", fmt.Errorf(`value '%v' must be a string conforming to regex "^[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[4][a-fA-F0-9]{3}-[8|9|aA|bB][a-fA-F0-9]{3}-[a-fA-F0-9]{12}$"`, this.UserId))
-	}
-	if !_regex_OuterMessage_UserIdX.MatchString(this.UserIdX) {
-		return go_proto_validators.FieldError("UserIdX", fmt.Errorf(`value '%v' must be a string conforming to regex "^[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[1-5][a-fA-F0-9]{3}-[8|9|aA|bB][a-fA-F0-9]{3}-[a-fA-F0-9]{12}$"`, this.UserIdX))
 	}
 	return nil
 }
